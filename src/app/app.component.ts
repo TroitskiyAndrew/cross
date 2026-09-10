87699b6c61c598933c93c76efefbe7a0f7a17dd5
AndreyTroitskiy <TroitskiyA@yandex.ru>
2026-09-11 02:07:46 +0700

    feat: create game board

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'cross';
+  board: Array<'X' | 'O' | null> = Array(9).fill(null);
+  currentPlayer: 'X' | 'O' = 'X';
+
+  handleCellClick(index: number): void {
+    if (this.board[index] !== null) {
+      return;
+    }
+
+    this.board[index] = this.currentPlayer;
+    this.currentPlayer = this.currentPlayer === 'X' ? 'O' : 'X';
+  }
 }
